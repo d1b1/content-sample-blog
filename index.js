55cd18601cb7d8755187509d8f657526916d7f82
Stephan L. Smith <Stephan.Smith.BC93@gmail.com>
2014-03-03 12:23:23 -0500

    Updated the layout to allow faster loading.

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ var argv    = require('optimist').argv;
 var port = argv.port || process.env.PORT || 5000;
 var app = express()
 
-// app.set("port", port);
 app.set("views", __dirname + "/views");
 app.set("view options", { layout: false, pretty: true });
 app.set("view engine", "jade");
-app.use('/assets', express.static(__dirname + '/assets'))
 
 app.get('/', function(req, res) {
     var opts = {url: 'http://tosheroon.herokuapp.com/blog/search', json:true}
@@ -28,6 +26,56 @@ app.get('/post/:id', function(req, res) {
 	  }
 	})
 })
+
+app.use(app.router);
+
+var assets = {
+    "style.css" : {
+        type: "css",
+        dir: "css",
+        files: [
+            "bootstrap.css",
+            "bootstrap-responsive.css",
+            "custom.css"
+        ]
+    },
+    "javascript.js" : {
+        type: "js",
+        dir: "js",
+        files: [
+            "jquery.js",
+            "jquery.cookie.js",
+            "bootstrap-typeahead.js",
+            "bootstrap-dropdown.js",
+            "bootstrap-collapse.js",
+            "bootstrap-tab.js",
+            "bootstrap-affix.js",
+            "bootstrap-modal.js",
+            "bootstrap-carousel.js",
+            "bootstrap-tooltip.js",
+            "underscore.js"
+        ]
+    }
+}
+
+var assetManagerConfig = {
+	env         : "production",
+    rootRoute   : "/static",
+    srcDir      : "./assets",
+    buildDir    : "./builtAssets",
+    process     : "true"
+};
+
+app.use(require("express-asset-manager")(assets, assetManagerConfig));
+
+app.configure('development', function() {
+    app.use('/assets', express.static(__dirname + '/assets'))
+});
+
+// in production, use a reverse proxy instead
+app.configure('production', function() {
+    app.use('/assets', express.static(__dirname + '/builtAssets'))
+});
 
 app.listen(port)
 console.log('Starting Sample Blog on ' + port)
